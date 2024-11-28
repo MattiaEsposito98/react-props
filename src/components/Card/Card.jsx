18a9408882548501bd951551172c09e8a4eb763c
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-28 15:43:45 +0100

    add metodo map per la card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
 import style from './Card.module.css'
 
-export default function PostCard() {
+export default function Card({ className = '', title = '', content = '', image, tags = [] }) {
+  const tagsPost = tags.join(',')
+
   return (
     <div className={style.card}>
       <img className={style.image} src="https://picsum.photos/200/300" alt="" />
       <div className={style.card_body}>
         <h3 className={style.card_title}>Titolo del post</h3>
+        <p> {tagsPost} </p>
         <p className={style.card_description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus provident fuga quas eligendi deleniti officiis sint minus sed nemo perspiciatis.</p>
         <button className={style.button}>Leggi di più </button>
       </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,23 @@
 import Card from '../Card/Card'
 import style from './Main.module.css'
+import posts from '../posts'
 
 export default function Main() {
   return (
     <main>
       <section className={style.section}>
         <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <Card />
-            </div>
-
-          </div>
+          {
+            posts.length ?
+              <div className="row">
+                {posts.map((post) => (
+                  <div key={post.id} className="col-6" >
+                    <Card title={post.title} tags={post.tags} image={post.image} />
+                  </div>
+                ))}
+              </div> :
+              <p>Non ci sono post</p>
+          }
         </div>
       </section>
     </main>
